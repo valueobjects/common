7fa10578a49faf54331efac0449c3cd03ffa0a08
Mehmet ÖĞMEN <e-posta@mehmetogmen.com.tr>
2020-10-09 03:49:34 +0300

    Project init.

---
+++ src/AbstractValueObject.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace ValueObjects\Common;
+
+abstract class AbstractValueObject
+{
+    public static function getValueObjectName(): string
+    {
+        return substr(strrchr(static::class, "\\"), 1);
+    }
+}
---
+++ src/ValueObjectInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace ValueObjects\Common;
+
+use \JsonSerializable;
+
+interface ValueObjectInterface extends JsonSerializable
+{
+    public static function getValueObjectName(): string;
+
+
+    public function __toString(): string;
+}

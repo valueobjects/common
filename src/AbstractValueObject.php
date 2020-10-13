b6e9b3463a77d9634e88be3c57eb5c37e14d7e18
Mehmet ÖĞMEN <e-posta@mehmetogmen.com.tr>
2020-10-13 22:55:50 +0300

    AbstractValueObject __destruct method remove.

--- src/AbstractValueObject.php
+++ src/AbstractValueObject.php
@@ -4,15 +4,8 @@ namespace ValueObjects\Common;
 
 abstract class AbstractValueObject implements ValueObjectInterface
 {
-    protected $observerManager;
-
     public static function getValueObjectName(): string
     {
         return substr(strrchr(static::class, '\\'), 1);
-    }
-
-    public function __destruct()
-    {
-        $this->observer()->notify('destroy');
     }
 }
